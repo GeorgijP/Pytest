595c807a163ba1ae0019f06434fc1a547bdd74af
GeorgijP <georgij.puzanov@inbox.ru>
2023-01-28 17:36:09 +0400

    Добавил fixture

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,18 +1,24 @@
+import pytest
 from utils import arrs, dicts
 
 
-def test_get():
-    assert arrs.get([1, 2, 3], 1, "test") == 2
+@pytest.fixture
+def variable():
+    return [1, 2, 3, 4]
+
+
+def test_get(variable):
+    assert arrs.get(variable, 1, "test") == 2
     assert arrs.get([], -1, "test") == "test"
 
 
-def test_slice():
-    assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
-    assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
-    assert arrs.my_slice([1, 2, 3], -1) == [3]
+def test_slice(variable):
+    assert arrs.my_slice(variable, 1, 3) == [2, 3]
+    assert arrs.my_slice(variable, 1) == [2, 3, 4]
+    assert arrs.my_slice(variable, -1) == [4]
     assert arrs.my_slice([], 1) == []
-    assert arrs.my_slice([1, 2, 3], 0) == [1, 2, 3]
-    assert arrs.my_slice([1, 2, 3], -4, 0) == []
+    assert arrs.my_slice(variable, 0) == [1, 2, 3, 4]
+    assert arrs.my_slice(variable, -5, 0) == []
 
 
 def test_get_val():
